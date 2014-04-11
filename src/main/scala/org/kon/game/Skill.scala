256c0522f3b27fe13d80d4382bc8478579511bd8
kongus99 <kongus99@gmail.com>
2014-04-11 15:09:38 +0200

    Scala skills ver 2

--- src/main/scala/org/kon/game/Skill.scala
+++ src/main/scala/org/kon/game/Skill.scala
@@ -3,39 +3,54 @@ sealed class SkillAdjustmentOutOfRange extends Exception
 abstract sealed class Skill {
   val MIN = 0
   val MAX = 3
-  val value = base + adjustment
 
   def base: Int
 
   def adjustment: Int
 
+  def sign: Int
+
+  def value = base + sign * adjustment
+
   def create(base: Int, adjustment: Int): Skill
 
-  def adjustDown: Skill = if (adjustment > MIN) create(base, adjustment - 1) else throw new SkillAdjustmentOutOfRange
+  def moveRight: Skill = if (adjustment > MIN) create(base, adjustment - 1) else throw new SkillAdjustmentOutOfRange
 
-  def adjustUp: Skill = if (adjustment < MAX) create(base, adjustment + 1) else throw new SkillAdjustmentOutOfRange
+  def moveLeft: Skill = if (adjustment < MAX) create(base, adjustment + 1) else throw new SkillAdjustmentOutOfRange
 }
 
 sealed case class SPEED(base: Integer, adjustment: Integer) extends Skill {
   override def create(base: Int, adjustment: Int): Skill = new SPEED(base, adjustment)
+
+  override def sign: Int = 1
 }
 
 sealed case class SNEAK(base: Integer, adjustment: Integer) extends Skill {
   override def create(base: Int, adjustment: Int): Skill = new SNEAK(base, adjustment)
+
+  override def sign: Int = -1
 }
 
 sealed case class FIGHT(base: Integer, adjustment: Integer) extends Skill {
   override def create(base: Int, adjustment: Int): Skill = new FIGHT(base, adjustment)
+
+  override def sign: Int = 1
 }
 
 sealed case class WILL(base: Integer, adjustment: Integer) extends Skill {
   override def create(base: Int, adjustment: Int): Skill = new WILL(base, adjustment)
+
+  override def sign: Int = -1
 }
 
 sealed case class LORE(base: Integer, adjustment: Integer) extends Skill {
   override def create(base: Int, adjustment: Int): Skill = new LORE(base, adjustment)
+
+  override def sign: Int = 1
 }
 
 sealed case class LUCK(base: Integer, adjustment: Integer) extends Skill {
   override def create(base: Int, adjustment: Int): Skill = new LUCK(base, adjustment)
+
+  override def sign: Int = -1
 }
--- src/main/scala/org/kon/game/Slider.scala
+++ src/main/scala/org/kon/game/Slider.scala
@@ -1,4 +1,17 @@
 abstract sealed class Slider(first: Skill, second: Skill) {
+  def create(first: Skill, second: Skill): Slider
 
+  def moveRight: Slider = create(first.moveRight, second.moveRight)
+
+  def moveLeft: Slider = create(first.moveLeft, second.moveLeft)
+
+  def value(s: Skill): Int = s match {
+    case first.getClass => first.value
+    case second.getClass => second.value
+  }
+}
+
+sealed case class SPEED_SNEAK(baseSpeed: Int, baseSneak: Int, adjustment: Int) extends Slider(SPEED(baseSpeed, adjustment), SNEAK(baseSneak, adjustment)) {
+  override def create(first: Skill, second: Skill): Slider = new SPEED_SNEAK(first.base, second.base, first.adjustment)
 }
 
