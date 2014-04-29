30753acd0952975d4afae2d804380bc041323126
kongus99 <kongus99@gmail.com>
2014-04-29 12:11:17 +0200

    focus tests added

--- src/test/scala/org/kon/game/SkillSetTest.scala
+++ src/test/scala/org/kon/game/SkillSetTest.scala
@@ -14,21 +14,11 @@ class SkillSetTest extends FunSpec {
     val skillSet: SkillSet = new SkillSet(Map(SPEED -> 1, SNEAK -> 3, FIGHT -> 3, WILL -> 5, LORE -> 4, LUCK -> 6), 5)
 
     it("should be readable") {
-      assert(1 === skillSet.value(SPEED))
-      assert(3 === skillSet.value(SNEAK))
-      assert(3 === skillSet.value(FIGHT))
-      assert(5 === skillSet.value(WILL))
-      assert(4 === skillSet.value(LORE))
-      assert(6 === skillSet.value(LUCK))
+      assertValuesCorrect(skillSet, 1, 3, 3, 5, 4, 6)
     }
     it("should be adjustable up to focus value to the right from starting position") {
       val adjusted = skillSet.moveRight(SPEED_SNEAK, 3).moveRight(FIGHT_WILL, 2)
-      assert(4 === adjusted.value(SPEED))
-      assert(0 === adjusted.value(SNEAK))
-      assert(5 === adjusted.value(FIGHT))
-      assert(3 === adjusted.value(WILL))
-      assert(4 === skillSet.value(LORE))
-      assert(6 === skillSet.value(LUCK))
+      assertValuesCorrect(adjusted, 4, 0, 5, 3, 4, 6)
     }
     it("should not be adjustable more than 3 times to the right from the starting position") {
       intercept[SkillAdjustmentOutOfRange] {
@@ -45,23 +35,40 @@ class SkillSetTest extends FunSpec {
         skillSet.moveRight(SPEED_SNEAK, 3).moveRight(FIGHT_WILL, 3)
       }
     }
+
+    it("should be adjustable more than focus value to the right from the starting position, provided we move back a little") {
+      val adjusted = skillSet.moveRight(SPEED_SNEAK, 3).moveRight(FIGHT_WILL, 2).moveLeft(SPEED_SNEAK, 2).moveRight(LORE_LUCK, 2)
+      assertValuesCorrect(adjusted, 2, 2, 5, 3, 6, 4)
+    }
+
     it("should be adjustable right and left") {
       val adjusted = skillSet.moveRight(SPEED_SNEAK, 3).moveLeft(SPEED_SNEAK, 2)
-      assert(2 === adjusted.value(SPEED))
-      assert(2 === adjusted.value(SNEAK))
+      assertValuesCorrect(adjusted, 2, 2, 3, 5, 4, 6)
     }
 
     it("should have the same values after finishing all moves") {
       val adjusted = skillSet.moveRight(SPEED_SNEAK, 3).moveLeft(SPEED_SNEAK, 2).finish
-      assert(2 === adjusted.value(SPEED))
-      assert(2 === adjusted.value(SNEAK))
+      assertValuesCorrect(adjusted, 2, 2, 3, 5, 4, 6)
+    }
+
+    it("should be possible to move up to focus after finishing move") {
+      val adjusted = skillSet.moveRight(SPEED_SNEAK, 3).moveRight(FIGHT_WILL, 2).finish.moveRight(FIGHT_WILL, 1).moveRight(LORE_LUCK, 3)
+      assertValuesCorrect(adjusted, 4, 0, 6, 2, 7, 3)
     }
 
     it("should should return to the base position after symmetrical moves") {
       val adjusted = skillSet.moveRight(SPEED_SNEAK).moveLeft(SPEED_SNEAK)
-      assert(1 === adjusted.value(SPEED))
-      assert(3 === adjusted.value(SNEAK))
+      assertValuesCorrect(adjusted, 1, 3, 3, 5, 4, 6)
     }
   }
 
+
+  def assertValuesCorrect(set: SkillSet, speed: Int, sneak: Int, fight: Int, will: Int, lore: Int, luck: Int) {
+    assert(speed === set.value(SPEED))
+    assert(sneak === set.value(SNEAK))
+    assert(fight === set.value(FIGHT))
+    assert(will === set.value(WILL))
+    assert(lore === set.value(LORE))
+    assert(luck === set.value(LUCK))
+  }
 }
