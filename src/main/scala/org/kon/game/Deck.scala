97ef8e02652247c942e7a51ef0d56f3921d97f91
kongus99 <kongus99@gmail.com>
2014-05-22 16:24:35 +0200

    Deck ver 1

---
+++ src/main/scala/org/kon/game/Deck.scala
@@ -0,0 +1,31 @@
+package org.kon.game
+
+class Deck[C](elements: Vector[C]) {
+
+  def drawFirst: (C, Deck[C]) = elements match {
+    case x :: tail => (x, new Deck(tail))
+    case Nil => throw new NotEnoughDeckElements
+  }
+
+  def shuffle: Deck[C] = new Deck(util.Random.shuffle(elements))
+
+  def remove(value: C, list: List[C]): List[C] = list diff List(value)
+
+  def draw(elem: C): (C, Deck[C]) = elements match {
+    case Nil => throw new NotEnoughDeckElements
+    case x :: tail => {
+      val found = elements.find(x => x == elem)
+      found match {
+        case Some(e) => (e, new Deck(remove(e, elements)))
+        case None => throw new DeckElementNotFound
+      }
+    }
+  }
+
+  def putAway(elem: C): Deck[C] = ???
+
+}
+
+class NotEnoughDeckElements extends RuntimeException
+
+class DeckElementNotFound extends RuntimeException
