632c686f44a1d5514f0bceb9ec81f8c4d7b3feb8
kongus99 <kongus99@gmail.com>
2014-05-23 16:00:50 +0200

    first investigator test - still failing

--- src/main/scala/org/kon/game/Investigator.scala
+++ src/main/scala/org/kon/game/Investigator.scala
@@ -1,12 +1,12 @@
 package org.kon.game
 
 
-class Investigator(possessions: List[Possession]) {
+class Investigator[T <: Possession](possessions: List[T]) {
 
-  def giveFixedPossessions(fixed: List[Possession], deck: Deck[Possession]): (Investigator, Deck[Possession]) =
-    (new Investigator(possessions ::: fixed), deck.draw(fixed))
+  def giveFixedPossessions(fixed: List[T], deck: Deck[T]): (Investigator[T], Deck[T]) =
+    (new Investigator[T](possessions ::: fixed), deck.draw(fixed))
 
-  def giveRandomPossessions(number: Int, deck: Deck[Possession]): (Investigator, Deck[Possession]) =
+  def giveRandomPossessions(number: Int, deck: Deck[T]): (Investigator[T], Deck[T]) =
     giveFixedPossessions(deck.peek(number), deck)
 
   def has(p: Possession): Boolean = possessions contains p
---
+++ src/test/scala/org/kon/game/InvestigatorTest.scala
@@ -0,0 +1,22 @@
+package org.kon.game
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSpec
+
+
+@RunWith(classOf[JUnitRunner])
+class InvestigatorTest extends FunSpec {
+  describe("An investigator without possession") {
+    val investigator: Investigator[Possession] = new Investigator[Possession](Nil)
+    val deck: Deck[Possession] = new Deck[Possession](new TestPossession(1) :: new TestPossession(1) :: new TestPossession(2) :: new TestPossession(3) :: Nil)
+    it("should receive fixed possessions from deck") {
+      val result = investigator.giveFixedPossessions(new TestPossession(1) :: new TestPossession(2) :: Nil, deck)
+      assert((result._1.has(new TestPossession(1)) &&
+        result._1.has(new TestPossession(2)) &&
+        !result._1.has(new TestPossession(3))) === true)
+    }
+  }
+}
+
+class TestPossession(v: Int) extends Possession
