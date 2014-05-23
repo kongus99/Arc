a003fb246a87e979af2447809075220fb4648fa0
kongus99 <kongus99@gmail.com>
2014-05-23 16:01:31 +0200

    first investigator test - still failing

--- src/test/scala/org/kon/game/InvestigatorTest.scala
+++ src/test/scala/org/kon/game/InvestigatorTest.scala
@@ -10,7 +10,7 @@ class InvestigatorTest extends FunSpec {
   describe("An investigator without possession") {
     val investigator: Investigator[Possession] = new Investigator[Possession](Nil)
     val deck: Deck[Possession] = new Deck[Possession](new TestPossession(1) :: new TestPossession(1) :: new TestPossession(2) :: new TestPossession(3) :: Nil)
-    it("should receive fixed possessions from deck") {
+    it("should receive fixed possessions from deck, from which the possessions are removed") {
       val result = investigator.giveFixedPossessions(new TestPossession(1) :: new TestPossession(2) :: Nil, deck)
       assert((result._1.has(new TestPossession(1)) &&
         result._1.has(new TestPossession(2)) &&
