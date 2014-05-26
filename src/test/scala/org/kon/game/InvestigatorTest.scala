8e226b0ab0e11b3674f4321d76d437fd52a7d660
kongus99 <kongus99@gmail.com>
2014-05-26 12:49:41 +0200

    investigator test - finished

--- src/test/scala/org/kon/game/InvestigatorTest.scala
+++ src/test/scala/org/kon/game/InvestigatorTest.scala
@@ -9,14 +9,23 @@ import org.scalatest.FunSpec
 class InvestigatorTest extends FunSpec with DeckTester {
   describe("An investigator without possession") {
     val investigator: Investigator[Possession] = new Investigator[Possession](Nil)
-    val deck: Deck[Possession] = new Deck[Possession](new TP(1) :: new TP(1) :: new TP(2) :: new TP(3) :: Nil)
+    val deck: Deck[Possession] = new Deck[Possession](new TP(1) :: new TP(2) :: new TP(3) :: new TP(1) :: Nil)
+
     it("should receive fixed possessions from deck, from which the possessions are removed") {
       val result = investigator.giveFixedPossessions(new TP(1) :: new TP(2) :: Nil, deck)
       val i = result._1
       val d = result._2
       assert((i.has(new TP(1)) && i.has(new TP(2)) && !i.has(new TP(3))) === true)
-      deckEquals(d, new TP(1) :: new TP(3) :: Nil)
+      deckEquals(d, new TP(3) :: new TP(1) :: Nil)
     }
+    it("should receive random possessions from top of the deck") {
+      val result = investigator.giveRandomPossessions(2, deck)
+      val i = result._1
+      val d = result._2
+      assert((i.has(new TP(1)) && i.has(new TP(2)) && !i.has(new TP(3))) === true)
+      deckEquals(d, new TP(3) :: new TP(1) :: Nil)
+    }
+
   }
 }
 
