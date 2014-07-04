d0d25ffa3ff2ef1d222e40f8613026a0aa1ece65
kongus99 <kongus99@gmail.com>
2014-07-04 13:43:38 +0200

    polishing

--- src/main/scala/org/kon/game/Investigator.scala
+++ src/main/scala/org/kon/game/Investigator.scala
@@ -4,10 +4,10 @@ package org.kon.game
 class Investigator[T <: Possession](possessions: List[T]) {
 
   def giveFixedPossessions(fixed: List[T], deck: Deck[T]): (Investigator[T], Deck[T]) =
-    (new Investigator[T](possessions ::: fixed), deck.draw(fixed))
+    (new Investigator[T](possessions ::: fixed), deck draw fixed)
 
   def giveRandomPossessions(number: Int, deck: Deck[T]): (Investigator[T], Deck[T]) =
-    giveFixedPossessions(deck.peek(number), deck)
+    giveFixedPossessions(deck peek number, deck)
 
   def has(p: Possession): Boolean = possessions contains p
 }
