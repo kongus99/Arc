39d36f422141520a57530fcb5e40b5714dae4e86
kongus99 <kongus99@gmail.com>
2014-10-27 13:53:51 +0100

    movement now is limited by path obstructions

--- src/main/scala/org/kon/game/phase/Movement.scala
+++ src/main/scala/org/kon/game/phase/Movement.scala
@@ -4,8 +4,6 @@ import org.kon.game.player.{Investigator, Possession}
 
 class Movement(path: List[Int]) {
   def isValid(investigator: Investigator[Possession], board: Board1[Int]): Boolean =
-    investigator.hasEnoughMovement(path) &&
-      board.pathIsConnected(investigator.currentPosition, path) &&
-      board.noPathObstructions(path)
+    investigator.hasEnoughMovement(path) && board.pathIsConnected(investigator.currentPosition, path) && board.noPathObstructions(path)
 
 }
