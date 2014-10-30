f1dc2931756e92a3c93aeb5addaa029d14143508
kongus99 <kongus99@gmail.com>
2014-10-30 15:07:36 +0100

    removed board generics

--- src/main/scala/org/kon/game/phase/Board1.scala
+++ src/main/scala/org/kon/game/phase/Board1.scala
@@ -1,24 +1,24 @@
 package org.kon.game.phase
 
-class Board1[T](mappings: List[Pair[T, T]], obstructions: List[T]) {
+class Board1(mappings: List[Pair[Int, Int]], obstructions: List[Int]) {
 
-  private val value: Map[T, Set[T]] = {
+  private val value: Map[Int, Set[Int]] = {
     val reversed = mappings.map(e => (e._2, e._1))
     (mappings ::: reversed).groupBy(e => e._1).mapValues(e => e.map(x => x._2).toSet)
   }
 
-  def noPathObstructions(path: List[T]): Boolean = !containsObstructions(path)
+  def noPathObstructions(path: List[Int]): Boolean = !containsObstructions(path)
 
-  def pathIsConnected(start: T, path: List[T]): Boolean =
-    path.foldLeft((start, true): Pair[T, Boolean])((a: Pair[T, Boolean], b: T) => (b, contains(a._1, b) && a._2))._2
+  def pathIsConnected(start: Int, path: List[Int]): Boolean =
+    path.foldLeft((start, true): Pair[Int, Boolean])((a: Pair[Int, Boolean], b: Int) => (b, contains(a._1, b) && a._2))._2
 
-  def contains(pair: Pair[T, T]): Boolean =
+  def contains(pair: Pair[Int, Int]): Boolean =
     value.get(pair._1) match {
       case None => false
       case Some(f) => f.contains(pair._2)
     }
 
-  private def containsObstructions(path: List[T]): Boolean = path match {
+  private def containsObstructions(path: List[Int]): Boolean = path match {
     case Nil => false
     case h :: tail => obstructions.contains(h) || containsObstructions(tail)
   }
--- src/main/scala/org/kon/game/phase/Movement.scala
+++ src/main/scala/org/kon/game/phase/Movement.scala
@@ -3,7 +3,7 @@ package org.kon.game.phase
 import org.kon.game.player.{Investigator, Possession}
 
 class Movement(path: List[Int]) {
-  def isValid(investigator: Investigator[Possession], board: Board1[Int]): Boolean =
+  def isValid(investigator: Investigator[Possession], board: Board1): Boolean =
     investigator.hasEnoughMovement(path) && board.pathIsConnected(investigator.currentPosition, path) && board.noPathObstructions(path)
 
 }
